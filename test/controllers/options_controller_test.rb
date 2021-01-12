e279ba03d55fc9e628bdf771409bc1e671af0e77
sanjibroy360 <sanjibroy0098@gmail.com>
2021-01-12 16:56:16 +0530

    Create poll and display list of polls

--- test/controllers/options_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class OptionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get options_create_url
-    assert_response :success
-  end
-end
